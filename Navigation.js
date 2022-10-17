e4782eea10f76305653b09943ab0eccf39eec4f8
PraveenTSG <it20172978@my.sliit.lk>
2022-10-17 18:38:12 +0530

    FE - Client Home, Booked Appointments  pages implemented

--- Navigation.js
+++ Navigation.js
@@ -4,6 +4,8 @@ import { createStackNavigator } from "@react-navigation/stack";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import AppointmentListScreen from "./screens/Counsellor/AppointmentListScreen";
+import ClientHomeScreen from "./screens/Client/HomeScreen";
+import ClientBookedAppointmentsScreen from "./screens/Client/BookedAppointmentsScreen";
 
 const Stack = createStackNavigator();
 const screenOptions = { headerShown: false };
@@ -11,11 +13,16 @@ const SignedInStack = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="LoginScreen"
+        initialRouteName="BookedAppointments"
         screenOptions={screenOptions}
       >
         <Stack.Screen name="LoginScreen" component={LoginScreen} />
         <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+        <Stack.Screen name="ClientHomeScreen" component={ClientHomeScreen} />
+        <Stack.Screen
+          name="BookedAppointments"
+          component={ClientBookedAppointmentsScreen}
+        />
         <Stack.Screen
           name="AppointmentListScreen"
           component={AppointmentListScreen}
